58a56e24f4f25769ee31ecadad47ab2c8262f46a
chiefgit <kameyaw14@gmail.com>
2024-11-08 12:48:40 +0000

    changed from bcrypt to bcryptjs

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,5 +1,5 @@
 import validator from "validator";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import UserModel from "../models/UserModel.js";
 import jwt from "jsonwebtoken";
 import { v2 as cloudinary } from "cloudinary";
